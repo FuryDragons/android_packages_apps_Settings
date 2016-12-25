1652b58cc365e6453c910c143312a39bb6e1605d
Nyks45 <winglow45@gmail.com>
2016-12-25 12:49:33 +0300

    Rework Slider
    -Add Missing Switch
    Referenced Commit
    https://github.com/XOSP-Project/android_packages_apps_Settings/commit/7095cafd1441c8f7fc5a61313f020ad953e98728

--- src/com/android/settings/fds/MainSettingsLayout.java
+++ src/com/android/settings/fds/MainSettingsLayout.java
@@ -1,14 +1,20 @@
-/*Copyright (C) 2015 The ResurrectionRemix Project
-     Licensed under the Apache License, Version 2.0 (the "License");
-     you may not use this file except in compliance with the License.
-     You may obtain a copy of the License at
-          http://www.apache.org/licenses/LICENSE-2.0
-     Unless required by applicable law or agreed to in writing, software
-     distributed under the License is distributed on an "AS IS" BASIS,
-     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     See the License for the specific language governing permissions and
-     limitations under the License.
-*/
+/*
+ * Copyright (C) 2017 Fury Dragons Rom Project
+ * Copyright (C) 2016 The Xperia Open Source Project
+ * Copyright (C) 2015 crDroid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.android.settings.fds;
 
 import android.app.Fragment;
@@ -28,7 +34,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 import com.android.settings.util.AbstractAsyncSuCMDProcessor;
 import com.android.settings.util.CMDProcessor;
@@ -38,32 +43,51 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 import com.android.settings.fds.input.ButtonSettings;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import android.preference.CheckBoxPreference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
+import android.support.v4.view.PagerTabStrip;
+import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.android.internal.logging.MetricsLogger;
 
-public class MainSettingsLayout extends SettingsPreferenceFragment {
-    private static final String TAG = "MainSettingsLayout";
+public class MainSettingsLayout  extends SettingsPreferenceFragment {
+
+    PagerTabStrip mPagerTabStrip;
     ViewPager mViewPager;
+
+    String titleString[];
+
     ViewGroup mContainer;
-    PagerSlidingTabStrip mTabs;
-    SectionsPagerAdapter mSectionsPagerAdapter;
-	
- 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-   		 mContainer = container;
+
+    static Bundle mSavedState;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mContainer = container;
 
         View view = inflater.inflate(R.layout.fds_main, container, false);
-        mViewPager = (ViewPager) view.findViewById(R.id.viewpager);
-        mTabs = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        mTabs.setViewPager(mViewPager);
-        setHasOptionsMenu(true);
+        mViewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        mPagerTabStrip = (PagerTabStrip) view.findViewById(R.id.pagerTabStrip);
+        mPagerTabStrip.setDrawFullUnderline(true);
 
-		return view;
+        StatusBarAdapter StatusBarAdapter = new StatusBarAdapter(getFragmentManager());
+        mViewPager.setAdapter(StatusBarAdapter);
+		setHasOptionsMenu(true);
+
+        return view;
     }
 
-    public boolean onPreferenceChange(Preference preference, Object objValue) 		{
-
-        return true;
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // After confirming PreferenceScreen is available, we call super.
+        super.onActivityCreated(savedInstanceState);
     }
 
  @Override
@@ -81,29 +105,33 @@ public class MainSettingsLayout extends SettingsPreferenceFragment {
                 return false;
         }
     }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
+	
     @Override
     public void onSaveInstanceState(Bundle saveState) {
         super.onSaveInstanceState(saveState);
     }
 
-    class SectionsPagerAdapter extends FragmentPagerAdapter {
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 
+    class StatusBarAdapter extends FragmentPagerAdapter {
         String titles[] = getTitles();
         private Fragment frags[] = new Fragment[titles.length];
 
-        public SectionsPagerAdapter(FragmentManager fm) {
+        public StatusBarAdapter(FragmentManager fm) {
             super(fm);
 			frags[0] = new FdsAboutRom();
             frags[1] = new ButtonSettings();
             frags[2] = new CmPartsStatusBar();
             frags[3] = new StatusBarSettings();
-            frags[4] = new AnimationSettings();
+			frags[4] = new AnimationSettings();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
         }
 
         @Override
@@ -115,11 +143,6 @@ public class MainSettingsLayout extends SettingsPreferenceFragment {
         public int getCount() {
             return frags.length;
         }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return titles[position];
-        }
     }
 
     private String[] getTitles() {
@@ -129,13 +152,12 @@ public class MainSettingsLayout extends SettingsPreferenceFragment {
 			    getString(R.string.fds_button_title),
 				getString(R.string.fds_cmparts_title),
                 getString(R.string.fds_statusbar_title),
-				getString(R.string.fds_animations_title),};
-
+				getString(R.string.fds_animations_title),
+                    };
         return titleString;
     }
-
-    @Override
-    protected int getMetricsCategory() {
+    
+    protected int getMetricsCategory(){
         return MetricsEvent.APPLICATION;
-     }
+    }
 }
